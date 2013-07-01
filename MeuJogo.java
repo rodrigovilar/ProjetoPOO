4888437063dd53cd24f155b223bcbadcc48eeac3
Alisson Vinícius <vsouza4412@gmail.com>
2013-07-01 14:58:53 -0300

    Alisson

---
+++ MeuJogo.java
@@ -0,0 +1,33 @@
+package meujogo;
+
+public class MeuJogo {
+     public static void main(String[] args) {
+        Bonecos ze = new Bonecos();
+        ze.setNome("Zé");
+        ze.setPosicao(50,60);
+        ze.setIdade(5);
+        
+        Bonecos jao = new Bonecos();
+        jao.setNome("Jao");
+        jao.setPosicao(10,80);
+        jao.setIdade(3);
+        
+        
+        
+        
+        
+        
+       
+        
+        
+        
+        
+        
+    }
+         }
+    
+
+
+
+   
+
